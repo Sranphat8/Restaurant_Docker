477dde28b0859e296a4735e3373af77ccaedd3f3
Sranphat8 <654259001@webmail.npru.ac.th>
2025-07-27 16:50:32 +0700

    modify

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
